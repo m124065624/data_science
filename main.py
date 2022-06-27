3ea7f1d249996a6604142ede222a0aef2d75a4c3
m124065624 <m124065624@gmail.com>
2022-06-27 22:19:14 +0800

    final

--- main.py
+++ main.py
@@ -6,10 +6,36 @@ from gensim.models import KeyedVectors, word2vec, Word2Vec
 import multiprocessing
 import jieba
 import difflib
+import data_process
 
+# 数据源处理
+data_process.process()
+
+# 参数输入
 result_amount = 3
-output_amount = 10
-input_word = '采矿权人姓名'
+input_word = input("请输入待匹配串:\n")
+output_amount = int(input("请输入 top k:\n"))
+
+# 计算运行时间
+t1 = time.time()
+
+
+# stopwords
+def stopwordslist():
+    stopwords = [line.strip() for line in open('stopwords.txt', 'r', encoding='utf8').readlines()]
+    return stopwords
+
+
+stopwords = stopwordslist()
+
+# input_word 分词
+input_word_cut = []
+cuts = jieba.cut(input_word, cut_all=False)
+for cut in cuts:
+    if cut not in stopwords and cut != '\n':
+        input_word_cut.append(cut)
+
+# 将input_word加入数据源
 sentences = open(u'./data/result.txt')
 flag = 0
 for i in sentences:
@@ -18,17 +44,8 @@ for i in sentences:
 if flag == 0:
     open('data/result.txt', 'a', encoding='utf8').write(input_word + '\n')
 
-# 计算运行时间
-t1 = time.time()
-
-
-def stopwordslist():
-    stopwords = [line.strip() for line in open('stopwords.txt', 'r', encoding='utf8').readlines()]
-    return stopwords
-
-
+# 数据源分词
 sentences = open(u'./data/result.txt')
-stopwords = stopwordslist()
 sentences_cut = []
 for ele in sentences:
     cuts = jieba.cut(ele, cut_all=False)
@@ -37,7 +54,6 @@ for ele in sentences:
         if cut not in stopwords and cut != '\n':
             new_cuts.append(cut)
     sentences_cut.append(new_cuts)
-print(sentences_cut)
 
 # 分词后的文本保存在data.txt中
 with open('data.txt', 'w') as f:
@@ -53,35 +69,41 @@ with open('data.txt', 'w') as f:
                 result += i
         f.write(result + '\n')
 
+# 模型训练
 sentences = word2vec.Text8Corpus("data.txt")
-print(sentences)
 # size是神经网络的隐藏层单元数，也就是后续每个词向量的维度，默认为100
 model = Word2Vec(sentences, vector_size=256, min_count=1, window=5, sg=0, workers=multiprocessing.cpu_count())
 model.save('word2vec.model')
-print(model)
 
 # model.build_vocab(sentences, update=True)
 # model.train(sentences, total_examples=model.corpus_total_words, epochs=10)
 
-# get top 3 similar words
-similar = model.wv.most_similar([input_word], topn=result_amount)
-print(similar)
-similar_words = []
-for i in range(0, result_amount):
-    # print(i)
-    similar_words.append(similar[i][0])
-    # print(similar_words.append(similar[i][0]))
-# print(similar_words)
-
-t2 = time.time()
 final_result = []
-# get the sentences in result.txt
-for i in range(0, result_amount):
-    words = open(u'./data/result.txt')
-    for word in words:
-        if similar_words[i] in word:
-            final_result.append(word)
 
+# 取得相似的数据
+for i in range(0, len(input_word_cut)):
+    similar = model.wv.most_similar(str(input_word_cut[i]), topn=result_amount)
+    similar_words = []
+    for j in range(0, result_amount):
+        similar_words.append(similar[j][0])
+
+    # 使用word2vec算法找寻相似的数据
+    for j in range(0, result_amount):
+        words = open(u'./data/result.txt')
+        for word in words:
+            if similar_words[j] in word:
+                final_result.append(word)
+
+    # 找寻与input_word分词之后相同的数据
+    words = open(u'./data/result.txt')
+    for new_word in words:
+        if input_word_cut[i] in new_word:
+            final_result.append(new_word)
+
+# 数据去重
+final_result = list(set(final_result))
+
+# 计算相似度
 similarity = []
 for i in final_result:
     d = difflib.SequenceMatcher(None, input_word, str(i)).ratio() * 100
@@ -97,4 +119,6 @@ for t in max_number:
 for i in max_index:
     print(final_result[i])
 
-print("Loading time: %.4f" % (t2 - t1))
+# 计算运行时间
+t2 = time.time()
+print("Calculating time: %.4f" % (t2 - t1))
